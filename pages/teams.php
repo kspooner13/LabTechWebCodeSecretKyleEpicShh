b201ccb5653457d911c49374b389ff39c6948727
Kyle Spooner <kspooner13@yahoo.com>
2015-07-10 21:38:14 -0400

    Feature Enchancements to the HealthCheck Module

--- pages/teams.php
+++ pages/teams.php
@@ -1,7 +1,7 @@
 <?php
 
 //home.php  mainpage
-
+$dbclass = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 ?>
 
@@ -18,14 +18,232 @@
   <div class='row breadcrumbs'>
     <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
       <ul class='breadcrumbs'>
-        <li><a href='#'><i class='fa fa-home'></i></a></li>
-        <li><a href='#'>Dashboard</a></li>
+        <li><a href='#'><i class='fa fa-home blue'></i></a></li>
+        <li><a href='#'>Modules</a></li>
+		<li><a href='#'>Health Check</a></li>
       </ul>
     </div>
   </div>
   <!-- Breadcrumbs End -->
 <!-- Row Start -->
+<div class='row'>
 
+<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+
+  <!-- Row Start -->
+  <div class='row'>
+
+  <?php
+	// Lets do some calculations to get overall scores to look pretty
+	
+	$sql = "SELECT ROUND(AVG(AntiVirus), 1) AS AV,
+				ROUND(AVG(`disk`),1) AS `Disk`,
+				ROUND(AVG(Intrusion),1) AS Intrusion,
+				ROUND(AVG(Usability),1) AS Usability,
+				ROUND(AVG(Services),1) AS Services,
+				ROUND(AVG(`Event Log`),1) AS `Event Log`,
+				ROUND(AVG(Updates),1) AS Updates,
+				ROUND(
+				  ROUND(
+					ROUND(AVG(Antivirus))+
+					ROUND(AVG(`Disk`))+
+					ROUND(AVG(Intrusion))+
+					ROUND(AVG(Usability))+
+					ROUND(AVG(Services))+
+					ROUND(AVG(`Event Log`))+
+					ROUND(AVG(Updates)))/7, 2) AS `Overall Score` 
+		FROM hc_scores";
+	$calcs = $dbclass -> query($sql);
+	$info = $dbclass->fetch_array($calcs);
+
+	
+	
+	?>
+  	<div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'style='padding-left: 15%;'>
+
+	<?php //this does spacing things ?>
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Facebook -->
+      <div class='social-box facebook'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-bug'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['AV']; ?>%</span> AntiVirus
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Facebook End -->
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Twitter -->
+      <div class='social-box twitter'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-tasks'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Disk']; ?>%</span> Disks
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Twitter End -->
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Dribble -->
+      <div class='social-box dribbble'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-users'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Intrusion']; ?>%</span> Intrusion
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Dribble End -->
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Projects -->
+      <div class='social-box projects'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-thumbs-o-up'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Usability']; ?>%</span> Usability
+          </div>6
+        </div>
+      </div>
+      <!-- Social Box - Projects End -->
+
+    </div>
+
+  <!-- Row End -->
+
+</div>
+</div>
+
+<div class='row' style='align: center;'>
+
+<div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+
+	<div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'style='padding-left: 15%;'>
+
+	<?php //this does spacing things ?>
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Projects -->
+      <div class='social-box tasks'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-gears'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Services']; ?>%</span> Services
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Projects End -->
+
+    </div>
+    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Projects -->
+      <div class='social-box posts'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-wrench'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Updates'];?>%</span> Updates
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Projects End -->
+
+    </div>
+	    <div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Projects -->
+      <div class='social-box services'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-file-o'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Event Log'];?>%</span> Event Logs
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Projects End -->
+
+    </div>  
+	<div class='col-xs-12 col-sm-6 col-md-4 col-lg-2'>
+
+      <!-- Social Box - Projects -->
+      <div class='social-box overall'>
+
+        <div class='icon-container'>
+          <div class='inner'>
+            <i class='fa fa-wrench'></i>
+          </div>
+        </div>
+
+        <div class='text-container'>
+          <div class='inner'>
+            <span><?php echo $info['Overall Score'];?>%</span> Overall
+          </div>
+        </div>
+      </div>
+      <!-- Social Box - Projects End -->
+
+    </div>
+	
+
+  </div>
+</div>
+</div>
 
 <div class='row'>
 
