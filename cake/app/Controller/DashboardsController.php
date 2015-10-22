862c6b9758f9816fbe8b12a8b6e31656d44b2728
kspooner13 <kspooner13@yahoo.com>
2015-10-22 08:52:15 -0400

    10.5 fix + permissions fix

--- cake/app/Controller/DashboardsController.php
+++ cake/app/Controller/DashboardsController.php
@@ -51,20 +51,45 @@ class DashboardsController extends AppController {
             $this->Dashboard->setDataSource('default');
         }
             
-                $compCount = $this->Computer->query("SELECT count(*) FROM computers");
+                $compCount = $this->Computer->query("SELECT count(*) FROM computers JOIN usersec ON usersec.computerid = computers.computerid JOIN users ON users.userid = usersec.userid WHERE users.name = '".$_SESSION['Username']."'");
 		$this->set('compCount', $compCount);
                 
-                $clientCount = $this->Client->query("SELECT count(*) FROM Clients");
+                $clientCount = $this->Client->query("SELECT count(Clients.clientid) FROM Clients JOIN usersec on usersec.clientid = clients.clientid AND usersec.computerid = '0' JOIN users ON users.userid = usersec.userid WHERE users.name = '".$_SESSION['Username']."' ");
                 $this->set('clientCount', $clientCount);
                 
-                $offlineCount = $this->Computer->query("SELECT count(*) FROM computers WHERE (os LIKE '%server%' OR os LIKE '%linux%') AND
- ((computerid IN (SELECT computerid FROM heartbeatcomputers WHERE LastHeartbeatTime <  DATE_ADD(NOW(),INTERVAL -(SELECT heartbeatdbupdateinterval *3 FROM advancedconfig)SECOND))))");
+                $offlineCount = $this->Computer->query("SELECT count(*) as count FROM computers WHERE (os LIKE '%server%' OR os LIKE '%linux%') AND
+ ((computerid IN (SELECT heartbeatcomputers.computerid FROM heartbeatcomputers 
+ JOIN usersec ON usersec.computerid = heartbeatcomputers.computerid
+ JOIN users ON users.userid = usersec.userid
+WHERE users.name = '".$_SESSION['Username']."' AND LastHeartbeatTime <  DATE_ADD(NOW(),INTERVAL -(SELECT heartbeatdbupdateinterval *3 FROM advancedconfig)SECOND))))");
                 $this->set('offlineCount', $offlineCount);
                 
                 $ticketCount = $this->Ticket->query("SELECT count(*) FROM TICKETS WHERE `status`='1' OR `status`='2' AND externalid != NULL");
                 $this->set('ticketCount', $ticketCount);
                 
-                $clientDrop = $this->Client->find('list', array('fields' => array('Client.ClientID', 'Client.Name'), 'order' =>array('Client.Name ASC')));
+                $clientDrop = $this->Client->find('list', array('fields' => array('Client.ClientID', 'Client.Name'), 
+                                         'joins' => array(
+                array(
+                    'table' => 'usersec',
+                    'alias' => 'usersec',
+                    'type' => 'INNER',
+                    'conditions' => array(
+                        'usersec.clientid = client.clientid AND usersec.computerid =\'0\''
+                    )
+                ),
+                array(
+                    'table' => 'users',
+                    'alias' => 'users',
+                    'type' => 'INNER',
+                    'conditions' => array(
+                        'users.userid = usersec.userid '
+                    )
+                )
+               
+            ) ,
+                    
+                    'conditions' => array('users.name' => $_SESSION['Username']),
+                    'order' =>array('Client.Name ASC')));
                 $this->set('clientDrop', $clientDrop);
                         
                 $labtechServer = $this->Computer->query('SELECT * FROM computers WHERE computerid = 1');
@@ -80,7 +105,28 @@ class DashboardsController extends AppController {
 		}
 	public function view($clientid) {
 		if (!($client = $this->Client->find('first', array(
-        'conditions' => array('Client.ClientID' => $clientid))))) {
+                     'joins' => array(
+                array(
+                    'table' => 'usersec',
+                    'alias' => 'usersec',
+                    'type' => 'INNER',
+                    'conditions' => array(
+                        'usersec.clientid = client.clientid AND usersec.computerid =\'0\''
+                    )
+                ),
+                array(
+                    'table' => 'users',
+                    'alias' => 'users',
+                    'type' => 'INNER',
+                    'conditions' => array(
+                        'users.userid = usersec.userid '
+                    )
+                )
+               
+            ) ,
+        'conditions' => array('Client.ClientID' => $clientid, 'users.name' => 'mglunt')
+                    
+                    )))) {
 		throw new NotFoundException(__('Client not found'));
 		}
 		$this->set(compact('client'));
